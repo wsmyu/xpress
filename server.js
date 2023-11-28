8117d994fcba3c7665a4f262801dd29176bf8e3c
Wing Suet Michelle Yu <n01535631@humbermail.ca>
2023-11-28 15:07:24 -0500

    corrected port

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/login", async (req, res) => {
     res.status(500).send({ error: "Internal server error" });
   }
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
